8b0598d98bc4e51525b159baef3891cdf98a42e0
yi-jeong <o414_h@naver.com>
2022-06-02 15:38:43 +0900

    주소 변경

--- src/pages/Footer.js
+++ src/pages/Footer.js
@@ -6,13 +6,13 @@ function Footer(){
             <div className="container">
 
                 <ul className="quick">
-                    <li><Link to="/company">회사소개</Link></li>
+                    <li><Link to="/Service">회사소개</Link></li>
                     <li><Link to="/contact">고객문의</Link></li>
                 </ul>
 
                 <div className="info">
                     <span>(주)신사브릿지</span>
-                    <span>경기도 김포시 김포한강9로75번길 158 KR 이편한세상시티 한강신도시 A307</span>
+                    <span>서울특별시 중구 회현동 세종대로 14-1 그랜드센트럴 빌딩 9층, 11층</span>
                     <span>대표이사: 조은정</span>
                     <span>대표전화: 070-7847-6600</span>
                     <p className="copyright">© sinsabridge Corp. All rights reserved.</p>
