ead9cf809e4f1dcff60bb4a58318f3daac79736d
yi-jeong <o414_h@naver.com>
2022-06-04 21:41:54 +0900

    모바일 버전 css 수정

--- src/pages/main/MainCareers.js
+++ src/pages/main/MainCareers.js
@@ -1,7 +1,14 @@
+import { useEffect, useState } from 'react';
 import mainboximg from '../../assets/img/mainbox.png';
 import { Link } from 'react-router-dom';
 
 function MainCareers(){
+    const [ view, setView ] = useState('');
+
+    useEffect(()=>{
+        window.innerWidth < 765 ? setView(true) : setView(false);
+        console.log(view);
+    })
     return(
         <div className="mainCareers" data-aos="fade-up" data-aos-offset="200">
             <div className="container">
@@ -10,10 +17,17 @@ function MainCareers(){
                     <div className="content col-xl-5">
                         <h3 data-aos="fade-up">CAREERS</h3>
                         <h2 data-aos="fade-up">Right people for the 'sinsabridge'</h2>
+                        { view==true ?
+                        <p data-aos="fade-up">
+                            기술과 사람을 중심으로 새로운 세상, <br/>새로운 연결을 함께 하는<br/>
+                            sinsabridge와 동반 성장할<br/> 전문가를 기다립니다.
+                        </p>
+                        :
                         <p data-aos="fade-up">
                             기술과 사람을 중심으로 새로운 세상, 새로운 연결을 함께 하는<br/>
-                            sinsabridge 동반 성장할 전문가를 기다립니다.
+                            sinsabridge와 동반 성장할 전문가를 기다립니다.
                         </p>
+                        }
                         <Link to="/process" type="button" className="btn-type-black" data-aos="fade-up">영입절차 확인하기</Link>
                     </div>
 
